9fcb606e358fe37f9e187b61b8c07fa8b07f4c31
alevens1 <alevesn1@icloud.com>
2017-06-10 15:55:22 -0400

    Final Edition

---
+++ wordbank.js
@@ -0,0 +1,49 @@
+Words = {};
+Words.List = [];
+
+
+
+
+
+
+Words.List[0] = "maypop";
+Words.List[1] = "peony";
+Words.List[2] = "narcissus";
+Words.List[3] = "pansy";
+Words.List[4] = "zinnia";
+Words.List[5] = "sunflower";
+Words.List[6] = "rose";
+Words.List[7] = "cosmos";
+Words.List[8] = "vinca";
+Words.List[9] = "hyacinth";
+
+Words.List[10] = "geranium";
+Words.List[11] = "angelonia";
+Words.List[12] = "begonia";
+Words.List[13] = "hellebore";
+Words.List[14] = "amaranth";
+Words.List[15] = "impatien";
+Words.List[16] = "celosia";
+Words.List[17] = "marigold";
+Words.List[18] = "ageratum";
+Words.List[19] = "daisy";
+Words.List[20] = "purslane";
+Words.List[21] = "poppy";
+Words.List[22] = "cornflower";
+Words.List[23] = "gomphrena";
+
+Words.List[24] = "scabiosa";
+Words.List[25] = "lobelia";
+
+Words.List[26] = "snapdragon";
+Words.List[27] = "chrysanthemum";
+Words.List[28] = "torenia";
+Words.List[29] = "thunbergia";
+Words.List[30] = "calibrachoa";
+Words.List[31] = "petunia";
+Words.List[32] = "sage";
+Words.List[33] = "violet";
+Words.List[34] = "lily";
+
+Words.length = Words.List.length;
+
